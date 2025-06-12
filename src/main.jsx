831f940df4efcdb4bfd7413265269eb3e06beec3
zzeordyga <47037524+zzeordyga@users.noreply.github.com>
2025-06-12 19:01:20 +0700

    build: transform to react

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);

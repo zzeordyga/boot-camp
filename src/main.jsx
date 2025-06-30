bbb82e53cf050d20867a03632e7bf9ca9d139ac0
zzeordyga <47037524+zzeordyga@users.noreply.github.com>
2025-07-01 02:11:10 +0700

    build: adding changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ZiggyApp from './views/2101702761';
 import './style.css';
 import './tailwind.css';
 import { BrowserRouter, Route, Routes } from 'react-router';
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('app')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='/2101702761' element={<ZiggyApp/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

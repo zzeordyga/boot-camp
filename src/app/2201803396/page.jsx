3cdc9fb924e2b7f4636cd8bae8c3ddc3de8a9b07
Ebhin <abhiniwesa@gmail.com>
2025-07-03 16:00:01 +0700

    fix catFacts

--- src/app/2201803396/page.jsx
+++ src/app/2201803396/page.jsx
@@ -17,8 +17,8 @@ export default function Home(){
             <h2 className="text-4xl font-semibold text-pink-800">
                 Cat Facts!
             </h2>
-            <blockquote className="p-2 bg-green-800 border-red-600 text-yellow-300 rounded">
-                {catFacts.fact}
+            <blockquote className="p-2 border-red-600 text-yellow-300 rounded">
+                {catFacts}
             </blockquote>
             <button onClick={fetchCatFacts} className="bg-blue-400 hover:bg-blue-200 text-purple-500 px-4 py-1">
                 New Cat Facts

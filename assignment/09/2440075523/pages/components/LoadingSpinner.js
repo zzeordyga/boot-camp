067b32e1814486a039c5fdf40dbca35ffb8c56cf
Palent11414 <valentsetiawan1402@gmail.com>
2025-07-10 21:40:55 +0700

    Add Sesi-09 for Valent Setiawan - 24400765523

---
+++ assignment/09/2440075523/pages/components/LoadingSpinner.js
@@ -0,0 +1,8 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <p>Loading posts...</p>
+    </div>
+  );
+}

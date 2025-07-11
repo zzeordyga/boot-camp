61dba566d9887059706e6d7735bc6c95fba45be6
gayabarumalamselatan <soerjahartono22@gmail.com>
2025-07-11 23:32:37 +0700

    asg session 9 firebase febrian 2540118851

---
+++ src/assignment/09/2540118851/posts.js
@@ -0,0 +1,9 @@
+// lib/posts.js
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "./firebase";
+
+export async function fetchPosts() {
+  const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
+  const snapshot = await getDocs(q);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}

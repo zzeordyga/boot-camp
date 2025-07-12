ca7ce569579a12c11f408bb877d66325b78f5b03
Pearla Zahra M. <zmaharlika@gmail.com>
2025-07-12 13:08:48 +0700

    Add assignment only for 2440068423 - Pearla Zahra

---
+++ assignment/09/2440068423/posts/layout.js
@@ -0,0 +1,8 @@
+// src/app/assignment/09/2440068423/layout.jsx
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      {children}
+    </div>
+  );
+}
